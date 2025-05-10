1ce299c2c5a05403dc74566257dc9e57443d9c80
qiaoba <534042650@qq.com>
2025-05-11 02:45:13 +0800

    新增自动更换预测模式

--- zq.py
+++ zq.py
@@ -544,6 +544,21 @@ async def zq_settle(client, event):
             if variable.auto:
                 yss = query_records(type_id=None)
                 for ys in yss :
+                    if variable.balance <= 400000 :
+                        ys = query_records("stop")
+                        variable.continuous = ys["count"]
+                        variable.lose_stop = ys["field2"]
+                        variable.lose_once = ys["field3"]
+                        variable.lose_twice = ys["field4"]
+                        variable.lose_three = ys["field5"]
+                        variable.lose_four = ys["field6"]
+                        variable.initial_amount = ys["amount"]
+                        variable.auto = False
+                        mes = f"""启动 {ys["type"]}"""
+                        message = await client.send_message(config.group, mes, parse_mode="markdown")
+                        asyncio.create_task(delete_later(client, event.chat_id, event.id, 10))
+                        asyncio.create_task(delete_later(client, message.chat_id, message.id, 10))
+                        break
                     if (variable.balance*0.33) >= calculate_losses(ys["field2"],ys["amount"],ys["field3"],ys["field4"],ys["field5"],ys["field6"]):
                         variable.continuous = ys["count"]
                         variable.lose_stop = ys["field2"]
