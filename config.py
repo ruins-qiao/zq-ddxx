59ae65d2b1e8f5a931bcb9013cef8f2bea06b924
qiaoba <534042650@qq.com>
2025-06-10 20:49:59 +0800

    根据押注方式优化追投模式

--- config.py
+++ config.py
@@ -1,20 +1,16 @@
-api_id = '21674794'
-api_hash = '71565357858e9444acbba5c77b3dd0f8'
+api_id = ''
+api_hash = ''
 # 多账号启动是 注意用户认证文件名不能相同 会覆盖认证 已认证账号 设置问认证文件名
-user_session = 'user_session'
+user_session = ''
 #监控的群组
-group = -1002346599496
+group = []
 #监控的朱雀群组
-zq_group = [-1002262543959,-1001833464786]
+zq_group = []
 # 朱雀bot
-zq_bot = 5697370563
+zq_bot = 132
 # 自己id
-user = 7826617022
+user = 123
 # 转账统计使用
-name = "马卡龙"
+name = ""
 # 转账达到多少不打码
-top = 200000
-# 账户余额API配置（请替换为实际值）
-ZHUQUE_COOKIE = "socute=s%3Aec39756a588a4553b6690c75c152b759.LpuCDAI1a7ADDsqubMkTszCSojdMApo2u11DIG5eNSU"  # 替换为你的实际Cookie
-ZHUQUE_X_CSRF = "e521659262da607d94096db745d765cd"  # 替换为你的实际X-Csrf-Token
-ZHUQUE_API_URL = "https://zhuque.in/api/user/getInfo?"
+top = 123
