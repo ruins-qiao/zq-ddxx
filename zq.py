641d136adb78f525a547bcf87ad3c50565a0bb9c
qiaoba <534042650@qq.com>
2025-10-02 00:17:58 +0800

    修改项目环境信息

--- zq.py
+++ zq.py
@@ -381,7 +381,7 @@ def calculate_bet_amount(win_count, lose_count, initial_amount, lose_stop, lose_
         if lose_count == 3:
             return closest_multiple_of_500(variable.bet_amount * lose_three)
         if lose_count >= 4:
-            if (lose_count - 3) > variable.fierce_limit_count:
+            if (lose_count - 3) <= variable.fierce_limit_count:
                 # 计算猛押注金额
                 if (lose_count - 3) == 1:
                     variable.fierce_amount = variable.fierce_initial * variable.fierce_times[0]
